526ae30877c294dec0538e57a610ee93caa5e6b4
Michał Leśniewski <mlesniew@gmail.com>
2024-02-18 18:03:29 +0100

    Add ping-pong mechanism

--- src/schalter.cpp
+++ src/schalter.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <ArduinoOTA.h>
-#include <LittleFS.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266WiFiGratuitous.h>
+#include <LittleFS.h>
 #include <uri/UriRegex.h>
 
 #include <PicoUtils.h>
@@ -31,7 +32,6 @@ const char CONFIG_FILE[] PROGMEM = "/config.json";
 String hostname;
 String password;
 
-PicoUtils::Stopwatch last_command_stopwatch;
 std::vector<PicoUtils::Watch<bool>*> watches;
 PicoMQ picomq;
 
@@ -51,11 +51,50 @@ PicoUtils::PeriodicRun announce_state_proc(15, [] {
     idx = (idx + 1) % outputs.size();
 });
 
+class PingPong {
+    public:
+        PingPong(): pings(0), pongs(0) {
+            picomq.subscribe("calor/pong", [this](const char * payload) {
+                if (get_ping_pong_payload() == payload) {
+                    pong_stopwatch.reset();
+                    ++pongs;
+                }
+            });
+        }
+
+        void tick() {
+            if (ping_stopwatch.elapsed() >= 30) {
+                ++pings;
+                picomq.publish("calor/ping", get_ping_pong_payload());
+                ping_stopwatch.reset();
+            }
+        }
+
+        unsigned long pong_elapsed_millis() const {
+            return pong_stopwatch.elapsed_millis();
+        }
+
+        unsigned long get_pings() const { return pings; }
+        unsigned long get_pongs() const { return pongs; }
+
+    protected:
+        unsigned long pings, pongs;
+        PicoUtils::Stopwatch ping_stopwatch, pong_stopwatch;
+
+        String get_ping_pong_payload() const {
+            return hostname + " " + String(pings);
+        }
+};
+
+PingPong ping_pong;
+
 void setup_wifi() {
     PicoUtils::BackgroundBlinker bb(led_blinker);
 
     WiFi.hostname(hostname);
     WiFi.setAutoReconnect(true);
+    WiFi.setSleepMode(WIFI_NONE_SLEEP);
+    experimental::ESP8266WiFiGratuitous::stationKeepAliveSetIntervalMs(1000);
 
     Serial.println(F("Press button now to enter SmartConfig."));
     led_blinker.set_pattern(1);
@@ -96,6 +135,11 @@ void setup_wifi() {
         for (unsigned int idx = 0; idx < outputs.size(); ++idx) {
             json["relays"][idx] = outputs[idx]->get();
         }
+
+        auto pp = json["pingpong"];
+        pp["pings"] = ping_pong.get_pings();
+        pp["pongs"] = ping_pong.get_pongs();
+        pp["elapsed"] = ping_pong.pong_elapsed_millis() / 1000;
 
         server.sendJson(json);
     });
@@ -143,6 +187,8 @@ void setup() {
                        "\n\n"
                    ));
 
+    delay(3000);
+
     reset_button.init();
 
     LittleFS.begin();
@@ -161,7 +207,6 @@ void setup() {
     for (unsigned int idx = 0; idx < output_count; ++idx) {
         outputs.push_back(new PicoUtils::ShiftRegisterOutput(shift_register, idx));
         picomq.subscribe("schalter/" + board_id + "/" + String(idx) + "/set", [idx](String payload) {
-            last_command_stopwatch.reset();
             if (payload == "ON") {
                 outputs[idx]->set(true);
             } else if (payload == "OFF") {
@@ -189,7 +234,7 @@ void setup() {
 
 void update_status_led() {
     if (WiFi.status() == WL_CONNECTED) {
-        if (last_command_stopwatch.elapsed() <= 60) {
+        if (ping_pong.pong_elapsed_millis() <= 2 * 60 * 1000) {
             led_blinker.set_pattern(uint64_t(0b101) << 60);
         } else {
             led_blinker.set_pattern(uint64_t(0b1) << 60);
@@ -200,11 +245,21 @@ void update_status_led() {
     led_blinker.tick();
 };
 
+void restart_on_connectivity_loss() {
+    const unsigned long timeout = ping_pong.get_pongs() ? 3 * 60 * 1000 : 15 * 60 * 1000;
+
+    if (ping_pong.pong_elapsed_millis() >= timeout) {
+        ESP.restart();
+    }
+}
+
 void loop() {
     ArduinoOTA.handle();
     server.handleClient();
     picomq.loop();
+    ping_pong.tick();
     update_status_led();
     for (auto watch : watches) { watch->tick(); }
     announce_state_proc.tick();
+    restart_on_connectivity_loss();
 }
